f73af9e09af9106882790655ddaa3c77087339e5
Kent Worthington <kentw@dacotahpaper.com>
2023-12-01 14:40:53 -0600

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// modules and resources
+pub mod modules {
+    pub mod day_one;
+}
+
+
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+
+pub mod modules {
+    pub mod day_one;
+}
+
+use modules::day_one::day_one;
+
+fn main() {
+    println!("Day One: {:?}", day_one());
+}
